4518efe79fc52460b68d0d05930467883f5aea76
andreoliant <antonio.andreoli.meroni@gmail.com>
2019-01-01 17:32:51 +0100

    help for oc_asset + new asset from ap

--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -1,30 +1,62 @@
-# Aggiorna dati generali in data/
+# OC > Toolkit
+# Utility per aggiornare gli oc_asset in data/
+
+# MEMO:
+# il blocco va eseguito una volta sola in update di intero package oc
+# richiede load di progetti!
 
 # setup
 # library("usethis")
 # usethis::use_data_raw()
-
-matrix_comuni <- read_csv2("data-raw/matrix_comuni.csv")
-usethis::use_data(matrix_comuni, overwrite = TRUE)
-
-po_riclass <- read_csv2("data-raw/po_riclass.csv") %>%
-  # MEMO: raw contiene NA per i programmi POC 2014-2020 > fanno casino perché join con progetti è sempre su OC_COD_PROGRAMMA
-  filter(!is.na(OC_CODICE_PROGRAMMA))
-usethis::use_data(po_riclass, overwrite = TRUE)
-
-monithon_clp <- read_csv2("data-raw/monithon_clp.csv")
-usethis::use_data(monithon_clp, overwrite = TRUE)
-
 # progetti <- load_progetti(bimestre = bimestre, visualizzati=TRUE)
 # usethis::use_data(progetti, overwrite = TRUE)
 
 
 # ----------------------------------------------------------------------------------- #
-# Setup per query
+# etc
+
+# po_riclass
+po_riclass <- read_csv2("data-raw/po_riclass.csv") %>%
+  # MEMO: raw contiene NA per i programmi POC 2014-2020 > fanno casino perché join con progetti è sempre su OC_COD_PROGRAMMA
+  filter(!is.na(OC_CODICE_PROGRAMMA))
+usethis::use_data(po_riclass, overwrite = TRUE)
+
+# moniton_clp
+monithon_clp <- read_csv2("data-raw/monithon_clp.csv")
+usethis::use_data(monithon_clp, overwrite = TRUE)
+
+
+# ----------------------------------------------------------------------------------- #
+# matrix
+
+# matrix_comuni
+matrix_comuni <- read_csv2("data-raw/matrix_comuni.csv")
+usethis::use_data(matrix_comuni, overwrite = TRUE)
+# MEMO: questo viene da uno script a parte che va integrato in oc
+
+# matrix_op
+matrix_op <- read_csv2("data-raw/matrix_op.csv")
+usethis::use_data(matrix_op, overwrite = TRUE)
+
+# matrix_opos
+matrix_opos <- read_csv2("data-raw/matrix_opos.csv")
+usethis::use_data(matrix_opos, overwrite = TRUE)
+
+# matrix_ra_opos
+matrix_ra_opos <- read_csv2("data-raw/matrix_ra_opos.csv")
+usethis::use_data(matrix_ra_opos, overwrite = TRUE)
+
+# matrix_ra_temi_fsc
+matrix_ra_temi_fsc <- read_csv2("data-raw/matrix_ra_temi_fsc.csv")
+usethis::use_data(matrix_ra_temi_fsc, overwrite = TRUE)
+
+
+# ----------------------------------------------------------------------------------- #
+# query
+
 # TODO:
 # queste dovrebbero popolare in automatico il file "query_template.xlsx" (ora in "inst" con altri template xls)
 # creare wrapper che riepie direttamente il template
-# MEMO: il blocco va eseguito una volta sola in update di intero package oc (ma richiede upload di progetti)
 
 # crea matrix programmi > linee > azioni
 make_matrix_po <- function() {
@@ -47,6 +79,7 @@ make_matrix_po <- function() {
 
   write_delim(out, file.path("data-raw", "po_linee_azioni.csv"), delim = ";", na = "")
 }
+
 
 # crea matrix temi UE
 make_matrix_ue <- function() {
@@ -74,16 +107,27 @@ make_matrix_ue <- function() {
 
 }
 
-# workflow
+
+# categorie_cup
 categorie_cup <- read_csv2("data-raw/categorie_cup.csv")
 usethis::use_data(categorie_cup, overwrite = TRUE)
 
-make_matrix_po() # po_linee_azioni.csv
+# po_linee_azioni
+make_matrix_po()
 po_linee_azioni <- read_csv2("data-raw/po_linee_azioni.csv")
 usethis::use_data(po_linee_azioni, overwrite = TRUE)
 
-make_matrix_ue() # categorie_ue.csv
+# categorie_ue
+make_matrix_ue()
 categorie_ue <- read_csv2("data-raw/categorie_ue.csv")
 usethis::use_data(categorie_ue, overwrite = TRUE)
+
+# ra
+ra <- read_csv2("data-raw/ra.csv")
+usethis::use_data(ra, overwrite = TRUE)
+
+# aree_temi_fsc
+aree_temi_fsc <- read_csv2("data-raw/aree_temi_fsc.csv")
+usethis::use_data(aree_temi_fsc, overwrite = TRUE)
 
 
