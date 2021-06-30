ab712c5215512a84a2ff27a14edc26712c349056
andreoliant <antonio.andreoli.meroni@gmail.com>
2021-06-30 10:45:49 +0200

    spostamento di fix_progetti

--- R/setup_tools.R
+++ R/setup_tools.R
@@ -524,3 +524,85 @@ make_comuni <- function(file_name="matrix_comuni.csv") {
 
   write_delim(out, file.path(getwd(), "setup", "data-raw", file_name), delim = ";", na = "")
 }
+
+
+
+#' Fix temporaneo per il dataset progetti in vestione preesteso
+#'
+#' Integra il dataset.
+#'
+#' @param progetti Dataset in formato standard.
+#' @return Il dataset progetti integrato.
+fix_progetti <- function(progetti) {
+  
+  # fix temporaneo per matera
+  # progetti <- progetti %>%
+  #   mutate(OC_CODICE_PROGRAMMA = case_when(is.na(OC_CODICE_PROGRAMMA) ~ "2018MATERAFSC",
+  #                                          TRUE ~ OC_CODICE_PROGRAMMA),
+  #          OC_DESCRIZIONE_PROGRAMMA = case_when(is.na(OC_DESCRIZIONE_PROGRAMMA) ~ "MATERA CAPITALE DELLA CULTURA 2019",
+  #                                               TRUE ~ OC_DESCRIZIONE_PROGRAMMA))
+  # # progetti %>% filter(is.na(OC_CODICE_PROGRAMMA))
+  #
+  # progetti <- progetti %>%
+  #   mutate(DEN_REGIONE = case_when(COD_REGIONE == "002" ~ "VALLE D'AOSTA", # fix per denominazione bilingue
+  #                                  COD_REGIONE == "004" ~ "TRENTINO-ALTO ADIGE",
+  #                                  DEN_REGIONE == "EMILIA" ~ "EMILIA-ROMAGNA", # fix per denominazione doppia
+  #                                  DEN_REGIONE == "FRIULI" ~ "FRIULI-VENEZIA GIULIA",
+  #                                  TRUE ~ DEN_REGIONE))
+  
+  # progetti <- progetti %>%
+  #   mutate(FONDO_COMUNITARIO = case_when(OC_CODICE_PROGRAMMA == "2014IT16M2OP006" & is.na(FONDO_COMUNITARIO) ~ "FESR",
+  #                                        # MEMO: forzo su FESR ma c'è anche FSE
+  #                                        TRUE ~ FONDO_COMUNITARIO))
+  
+  # fix temporaneo per YEI
+  # progetti <- progetti %>%
+  #   mutate(FONDO_COMUNITARIO = case_when(FONDO_COMUNITARIO == "Y.E.I"~ "YEI",
+  #                                        TRUE ~ FONDO_COMUNITARIO))
+  
+  # fix temporaneo per IOG>YEI
+  progetti <- progetti %>%
+    mutate(FONDO_COMUNITARIO = case_when(FONDO_COMUNITARIO == "IOG" ~ "YEI",
+                                         OC_CODICE_PROGRAMMA == "2014IT05M9OP001" & is.na(FONDO_COMUNITARIO) ~ "YEI",
+                                         TRUE ~ FONDO_COMUNITARIO))
+  
+  # # fix temporaneo per ":::OC_CODICE_PROGRAMMA"
+  # progetti <- progetti %>%
+  #   mutate(OC_CODICE_PROGRAMMA = case_when(OC_CODICE_PROGRAMMA == ":::2014IT16RFOP007" ~ "2014IT16RFOP007",
+  #                                          OC_CODICE_PROGRAMMA == ":::2016POCIMPRESE1" ~ "2016POCIMPRESE1",
+  #                                          OC_CODICE_PROGRAMMA == ":::2017FSCRICERCA" ~ "2017FSCRICERCA",
+  #                                          OC_CODICE_PROGRAMMA == ":::2017POCRICERCA1" ~ "2017POCRICERCA1",
+  #                                          OC_CODICE_PROGRAMMA == ":::2017POIMPCOMFSC" ~ "2017POIMPCOMFSC",
+  #                                          TRUE ~ OC_CODICE_PROGRAMMA))
+  
+  # fix temporaneo per ":::OC_CODICE_PROGRAMMA" (su dati 20291031)
+  # progetti <- progetti %>%
+  #   mutate(OC_CODICE_PROGRAMMA = case_when(OC_CODICE_PROGRAMMA == ":::2014IT16RFOP007" ~ "2014IT16RFOP007",
+  #                                          OC_CODICE_PROGRAMMA == ":::2016POCIMPRESE1" ~ "2014IT16RFOP003", # cambia!
+  #                                          OC_CODICE_PROGRAMMA == ":::2017FSCRICERCA" ~ "2014IT16M2OP005",  # cambia!
+  #                                          OC_CODICE_PROGRAMMA == ":::2017POCRICERCA1" ~ "2014IT16M2OP005", # cambia!
+  #                                          OC_CODICE_PROGRAMMA == ":::2017POIMPCOMFSC" ~ "2017POIMPCOMFSC",
+  #                                          TRUE ~ OC_CODICE_PROGRAMMA))
+  
+  # fix temporaneo per ":::OC_CODICE_PROGRAMMA" (su dati 20291231)
+  # MEMO: sposto su programma SIE
+  # progetti <- progetti %>%
+  #   mutate(OC_CODICE_PROGRAMMA = case_when(OC_CODICE_PROGRAMMA == "2016POCIMPRESE1" & OC_COD_FONTE == "FS1420" ~ "2014IT16RFOP003",
+  #                                          OC_CODICE_PROGRAMMA == "2017FSCRICERCA" & OC_COD_FONTE == "FS1420" ~ "2014IT16M2OP005",
+  #                                          OC_CODICE_PROGRAMMA == "2017POCRICERCA1" & OC_COD_FONTE == "FS1420" ~ "2014IT16M2OP005",
+  #                                          TRUE ~ OC_CODICE_PROGRAMMA))
+  # 
+  # progetti <- progetti %>%
+  #   mutate(COD_LOCALE_PROGETTO = case_when(grepl("^1MISE174", COD_LOCALE_PROGETTO) ~ "1MISE174",
+  #                                          grepl("^1MISE397", COD_LOCALE_PROGETTO) ~ "1MISE397",
+  #                                          grepl("^1MISE496", COD_LOCALE_PROGETTO) ~ "1MISE496",
+  #                                          grepl("^1MISE608", COD_LOCALE_PROGETTO) ~ "1MISE608",
+  #                                          TRUE ~ COD_LOCALE_PROGETTO))
+  
+  # fix di progetti senza FONDO_COMUNITARIO
+  progetti <- progetti %>%
+    mutate(FONDO_COMUNITARIO = case_when(OC_CODICE_PROGRAMMA == "2014IT16M2OP002:::2016PATTIPUG" & is.na(FONDO_COMUNITARIO) ~ "FESR",
+                                         TRUE ~ FONDO_COMUNITARIO))
+  
+  return(progetti)
+}
